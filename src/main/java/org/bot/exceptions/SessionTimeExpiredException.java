dd8c310f05e03f1ca9835b2e86431e947eeff312
Margarita Yarmolchuk <margaritayarmolchuk@gmail.com>
2022-11-13 20:29:35 +0500

    Added SessionTimeExpiredException.

---
+++ src/main/java/org/bot/exceptions/SessionTimeExpiredException.java
@@ -0,0 +1,8 @@
+package org.bot.exceptions;
+
+public class SessionTimeExpiredException extends Exception {
+
+    public SessionTimeExpiredException(String message) {
+        super(message);
+    }
+}
