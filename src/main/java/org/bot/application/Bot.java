b00549f144ef3fb28fe1115ecf5eec21d49b497d
WoodieDudy <goshagks@yandex.ru>
2022-11-22 13:30:53 +0500

    feature: keyboard test example

--- src/main/java/org/bot/application/Bot.java
+++ src/main/java/org/bot/application/Bot.java
@@ -1,7 +1,6 @@
 package org.bot.application;
 
 import org.bot.domain.Mailbox;
-import org.bot.domain.Message;
 import org.bot.domain.User;
 import org.bot.domain.UserRepository;
 import org.bot.domain.commands.AuthCommand;
@@ -10,11 +9,23 @@ import org.bot.domain.commands.HelpCommand;
 import org.bot.domain.commands.LettersListCommand;
 import org.bot.enums.MessagesTemplates;
 import org.bot.infrastructure.interfaces.MailInterface;
+import org.checkerframework.checker.units.qual.A;
 import org.telegram.abilitybots.api.bot.AbilityBot;
 import org.telegram.abilitybots.api.objects.Ability;
+import org.telegram.telegrambots.meta.api.methods.ActionType;
+import org.telegram.telegrambots.meta.api.methods.send.SendChatAction;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.EditMessageReplyMarkup;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 
 import java.time.Duration;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Optional;
 
 import static org.telegram.abilitybots.api.objects.Locality.ALL;
 import static org.telegram.abilitybots.api.objects.Privacy.PUBLIC;
@@ -50,6 +61,37 @@ public final class Bot extends AbilityBot {
     public long creatorId() {
         return 123456789;
     }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        super.onUpdateReceived(update);
+        if (!update.hasCallbackQuery()) {
+            return;
+        }
+        System.out.println(update.getCallbackQuery().getData());
+
+        String callbackData = update.getCallbackQuery().getData();
+        String[] callbackDataParts = callbackData.split(" ");
+        String commandAlias = callbackDataParts[0];
+        String fromMessageId = callbackDataParts[1];
+        if (commandAlias.equals("letters")) {
+            InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
+            List<List<InlineKeyboardButton>> keyboard = new ArrayList<>();
+            List<InlineKeyboardButton> Buttons = new ArrayList<InlineKeyboardButton>();
+            InlineKeyboardButton github= new InlineKeyboardButton("thx!");
+            github.setCallbackData("letters 123");
+            Buttons.add(github);
+            keyboard.add(Buttons);
+            inlineKeyboardMarkup.setKeyboard(keyboard);
+
+            EditMessageReplyMarkup newKeyboard = new EditMessageReplyMarkup();
+            newKeyboard.setChatId(update.getCallbackQuery().getMessage().getChatId());
+            newKeyboard.setMessageId(Integer.parseInt(fromMessageId));
+            newKeyboard.setReplyMarkup(inlineKeyboardMarkup);
+            silent.execute(newKeyboard);
+        }
+    }
+
 
     public Ability start() {
         return Ability
@@ -102,7 +144,42 @@ public final class Bot extends AbilityBot {
                 .privacy(PUBLIC)
                 .action(ctx -> {
                     User user = userRepository.getUserById(ctx.chatId());
+                    SendChatAction sendChatAction = new SendChatAction(); // TODO move to BotInterface
+                    sendChatAction.setChatId(ctx.chatId());
+                    sendChatAction.setAction(ActionType.TYPING);
+                    silent.execute(sendChatAction);
                     silent.send(commands.get(abilityName).execute(user,  ctx.arguments()).getText(), ctx.chatId());
+                })
+                .build();
+    }
+
+    public Ability test() {
+        String abilityName = "test";
+        return Ability
+                .builder()
+                .name(abilityName)
+                .info("Prints letters")
+                .locality(ALL)
+                .privacy(PUBLIC)
+                .action(ctx -> {
+                    SendMessage message = new SendMessage();
+                    message.setChatId(ctx.chatId());
+                    message.setText("Inline model below.");
+
+                    InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
+                    List<List<InlineKeyboardButton>> keyboard = new ArrayList<>();
+                    List<InlineKeyboardButton> Buttons = new ArrayList<InlineKeyboardButton>();
+
+                    InlineKeyboardButton github = new InlineKeyboardButton("push me");
+                    github.setCallbackData("letters" + " " + (ctx.update().getMessage().getMessageId() + 1));
+                    Buttons.add(github);
+
+                    keyboard.add(Buttons);
+                    inlineKeyboardMarkup.setKeyboard(keyboard);
+                    message.setReplyMarkup(inlineKeyboardMarkup);
+
+                    Optional<Message> sys = silent.execute(message);
+                    sys.ifPresent(value -> System.out.println("sys" + value.getMessageId()));
                 })
                 .build();
     }
