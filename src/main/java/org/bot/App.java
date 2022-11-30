dd01d7880cb0c70b124d56f20fac4cd3e97f0f14
Margarita Yarmolchuk <margaritayarmolchuk@gmail.com>
2022-11-30 18:11:27 +0500

    feature: getting token for config

--- src/main/java/org/bot/App.java
+++ src/main/java/org/bot/App.java
@@ -2,20 +2,26 @@ package org.bot;
 
 import org.bot.application.Bot;
 import org.bot.infrastructure.JakartaMailInterface;
+import org.bot.infrastructure.PropertyParser;
 import org.bot.infrastructure.interfaces.MailInterface;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
+import java.io.IOException;
+import java.util.Properties;
+
 
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         MailInterface mailInterface = new JakartaMailInterface();
+
+        Properties properties = PropertyParser.getProperties("src/main/resources/config.properties");
 
         Bot bot = new Bot(
             mailInterface,
-            "",
-            ""
+            properties.getProperty("bot.token"),
+            properties.getProperty("bot.name")
         );
         try {
             TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
